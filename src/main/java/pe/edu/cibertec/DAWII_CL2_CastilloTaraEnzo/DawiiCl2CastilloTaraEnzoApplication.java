650b7f49796e6d25f2e1772e8fa7105a3c653b83
EFMCT30 <enzofranchesco.ec@gmail.com>
2023-10-28 19:25:38 -0500

    Primer commit, Diseño, Model

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL2_CastilloTaraEnzo/DawiiCl2CastilloTaraEnzoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL2_CastilloTaraEnzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2CastilloTaraEnzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2CastilloTaraEnzoApplication.class, args);
+	}
+
+}
